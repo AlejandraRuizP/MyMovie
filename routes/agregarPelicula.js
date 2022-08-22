f1306957f1ee05b7e129541cbe63de79d52f8a67
AlejandraRuizP <alejandraruizpimentel@gmail.com>
2022-08-22 13:23:46 -0400

    busqueda

--- routes/agregarPelicula.js
+++ routes/agregarPelicula.js
@@ -2,10 +2,12 @@ const express = require('express')
 
 
 const {getNewMovie,postNewMovie} = require('../controllers/agregarpelicula')
+const {verificarSesionCookie} = require('../middleware/auntenticacion')
+const {verificarPermiso} = require('../middleware/permisos')
 
 const router = express.Router()
 
-router.get('/',getNewMovie)
-router.post('/',postNewMovie)
+router.get('/',verificarSesionCookie,verificarPermiso,getNewMovie)
+router.post('/',verificarSesionCookie,verificarPermiso,postNewMovie)
 
 module.exports = router
